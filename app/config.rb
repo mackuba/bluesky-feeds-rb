df7f97f44f18545c60f4795e7fb7639df8340280
Kuba Suder <jakub.suder@gmail.com>
2023-06-15 22:47:50 +0200

    added some more comments

--- app/config.rb
+++ app/config.rb
@@ -8,3 +8,16 @@ BlueFactory.set :hostname, 'feeds.example.com'
 
 BlueFactory.add_feed 'linux', LinuxFeed.new
 BlueFactory.add_feed 'starwars', StarWarsFeed.new
+
+# do any additional config & customization on BlueFactory::Server here:
+#
+# BlueFactory::Server.disable :logging
+# BlueFactory::Server.set :port, 4000
+#
+# BlueFactory::Server.get '/' do
+#   redirect 'https://web.example.com'
+# end
+#
+# BlueFactory::Server.before do
+#   headers "X-Powered-By" => "BlueFactory/#{BlueFactory::VERSION}"
+# end
--- config.ru
+++ config.ru
@@ -1,10 +1,9 @@
 require_relative 'app/config'
 
+# might not be needed depending on the app server you use - comment out these lines to leave logs on STDOUT
 Dir.mkdir('log') unless Dir.exist?('log')
-
 log = File.new("log/sinatra.log", "a+")
 log.sync = true
-
 use Rack::CommonLogger, log
 
 run BlueFactory::Server
