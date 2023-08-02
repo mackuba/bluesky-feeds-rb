5a01f07cc82403ad8e2bc029c0d936809be3491c
Kuba Suder <jakub.suder@gmail.com>
2023-08-03 00:31:05 +0200

    mute spam about Mastodon etc.

--- app/feeds/linux_feed.rb
+++ app/feeds/linux_feed.rb
@@ -7,6 +7,19 @@ class LinuxFeed < Feed
     /apt\-get/, /\bflatpak\b/i, /\b[Xx]org\b/
   ]
 
+  EXCLUDE = [
+    /\bmastos?\b/i, /mast[oa]d[oa]n/i, /\bfederat(ion|ed)\b/i, /fediverse/i, /at\s?protocol/i,
+    /social (media|networks?)/i, /microblogging/i, /\bthreads\b/i, /\bnostr\b/i,
+    /the linux of/i, /linux (bros|nerds)/i, /ubuntu tv/i
+  ]
+
+  MUTED_PROFILES = [
+    'did:plc:35c6qworuvguvwnpjwfq3b5p',  # Linux Kernel Releases
+    'did:plc:ppuqidjyabv5iwzeoxt4fq5o',  # GitHub Trending JS/TS
+    'did:plc:eidn2o5kwuaqcss7zo7ivye5',  # GitHub Trending
+    'did:plc:lontmsdex36tfjyxjlznnea7',  # RustTrending
+  ]
+
   def feed_id
     2
   end
@@ -24,12 +37,15 @@ class LinuxFeed < Feed
   end
 
   def post_matches?(post)
-    REGEXPS.any? { |r| post.text =~ r }
+    return false if MUTED_PROFILES.include?(post.repo)
+
+    REGEXPS.any? { |r| post.text =~ r } && !(EXCLUDE.any? { |r| post.text =~ r })
   end
 
   def colored_text(t)
     text = t.dup
 
+    EXCLUDE.each { |r| text.gsub!(r) { |s| Rainbow(s).red }}
     REGEXPS.each { |r| text.gsub!(r) { |s| Rainbow(s).green }}
 
     text
