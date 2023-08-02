67d5a8ae195f5c5bf49adc8dc3782dd4525a939e
Kuba Suder <jakub.suder@gmail.com>
2023-08-03 00:30:13 +0200

    added more keywords to the Linux feed

--- app/feeds/linux_feed.rb
+++ app/feeds/linux_feed.rb
@@ -2,7 +2,9 @@ require_relative 'feed'
 
 class LinuxFeed < Feed
   REGEXPS = [
-    /linux/i, /debian/i, /ubuntu/i, /\bKDE\b/, /\bGTK\d?\b/
+    /linux/i, /debian/i, /ubuntu/i, /\bredhat\b/i, /\bRHEL\b/, /\bSUSE\b/, /\bCentOS\b/, /\bopensuse\b/i,
+    /\bslackware\b/i, /\bKDE\b/, /\bGTK\d?\b/, /#GNOME\b/, /\bGNOME\s?\d+/, /\bkde plasma\b/i,
+    /apt\-get/, /\bflatpak\b/i, /\b[Xx]org\b/
   ]
 
   def feed_id
