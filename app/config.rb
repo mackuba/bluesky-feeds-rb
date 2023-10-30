cd67022aba49f085f2289e7e16a756d788035d41
Kuba Suder <jakub.suder@gmail.com>
2023-10-30 21:15:46 +0200

    only require server.rb in the server scripts

--- app/config.rb
+++ app/config.rb
@@ -3,8 +3,6 @@ Dir[File.join(__dir__, 'feeds', '*.rb')].each { |f| require(f) }
 require 'blue_factory'
 require 'sinatra/activerecord'
 
-require_relative 'server'
-
 ActiveRecord::Base.connection.execute "PRAGMA journal_mode = WAL"
 
 BlueFactory.set :publisher_did, 'did:plc:<your_identifier_here>'
@@ -13,5 +11,3 @@ BlueFactory.set :hostname, 'feeds.example.com'
 BlueFactory.add_feed 'build', BuildInPublicFeed.new
 BlueFactory.add_feed 'linux', LinuxFeed.new
 BlueFactory.add_feed 'starwars', StarWarsFeed.new
-
-Server.configure
