1bff09cadd36daf6de0ddb65da546ba00cea9f3c
Kuba Suder <jakub.suder@gmail.com>
2023-10-30 22:39:29 +0200

    delete dependent FeedPosts when Post is deleted

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,8 @@ class Post < ActiveRecord::Base
   validates_presence_of :repo, :time, :data, :rkey
   validates :text, length: { minimum: 0, allow_nil: false }
 
+  has_many :feed_posts, dependent: :destroy
+
   attr_writer :record
 
   def self.find_by_at_uri(uri)
