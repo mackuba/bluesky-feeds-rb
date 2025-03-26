c7f8d86231dec7c48392fe160b2b15e2557d6030
Kuba Suder <jakub.suder@gmail.com>
2025-03-27 00:07:46 +0200

    enable YJIT or print some logs about it

--- app/init.rb
+++ app/init.rb
@@ -1,4 +1,23 @@
 require 'blue_factory'
 require 'sinatra/activerecord'
 
+if defined?(RubyVM::YJIT) && RubyVM::YJIT.respond_to?(:enabled?)
+  if !RubyVM::YJIT.enabled?
+    if RubyVM::YJIT.respond_to?(:enable)
+      # Ruby 3.3+
+      RubyVM::YJIT.enable
+    else
+      # Ruby 3.2
+      puts "-" * 106
+      puts "Note: YJIT is not enabled. To improve performance, enable it by adding an ENV var RUBYOPT=\"--enable-yjit\"."
+      puts "-" * 106
+    end
+  end
+else
+  puts "-" * 112
+  puts "Note: YJIT is not enabled. To improve performance, it's recommended to " +
+    ((RUBY_VERSION.to_f >= 3.2) ? "install Ruby with YJIT support turned on." : "update to a newer Ruby with YJIT support.")
+  puts "-" * 112
+end
+
 ActiveRecord::Base.connection.execute "PRAGMA journal_mode = WAL"
