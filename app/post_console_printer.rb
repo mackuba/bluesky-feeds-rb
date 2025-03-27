3d040bacd4d84a10b8a8bf1103dbe637d99b0324
Kuba Suder <jakub.suder@gmail.com>
2025-03-27 17:08:49 +0200

    show language label for non-English posts in print_feed

--- app/post_console_printer.rb
+++ app/post_console_printer.rb
@@ -9,6 +9,15 @@ class PostConsolePrinter
 
   def display(post)
     print Rainbow(post.time).bold + ' * ' + Rainbow(post.id).bold + ' * '
+
+    langs = post.record['langs']
+    if langs.nil?
+      print '[nil] * '
+    elsif langs != ['en']
+      label = langs.map { |ln| ln.upcase.tr('A-Z', "\u{1F1E6}-\u{1F1FF}") }.join
+      print "#{label}  * "
+    end
+
     puts Rainbow("https://bsky.app/profile/#{post.repo}/post/#{post.rkey}").darkgray
     puts
     puts @feed.colored_text(post.text)
